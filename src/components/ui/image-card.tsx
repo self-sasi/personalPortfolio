d7d8bb3129934b7f632d3533f0bd4c37e6698eb3
self-sasi <sarthaks281bond@gmail.com>
2026-01-11 03:00:36 -0700

    remove next image usage and update project links

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export function ImageCard() {
   return (
-    <Card className="relative h-full overflow-hidden">
-      <Image
-        src="/images/misc/mrbean.jpg"
-        alt="Mr Bean"
-        fill
-        className="object-cover"
-      />
+    <Card className="relative h-60 md:h-full overflow-hidden">
+      <img src="/images/misc/mrbean.jpg" className="absolute inset-0 h-full w-full object-cover" />
     </Card>
   );
 }
