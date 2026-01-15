1a794074177c180911c58b8caf6aabbf417a2094
self-sasi <sarthaks281bond@gmail.com>
2026-01-15 09:07:41 -0700

    add alert popup to skills card button mentioning it is in development

--- src/components/ui/skill-card.tsx
+++ src/components/ui/skill-card.tsx
@@ -3,22 +3,21 @@
 import {
   Card,
   CardAction,
-  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "./button";
 import { ArrowUpRightIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 export function SkillCard() {
+  const showAlert: () => void = () => alert("Under Construction! Please navigate to the Projects page to see my work and the tech involved.");
   return (
     <Card className="h-full bg-primary-turquoise text-primary-turquoise-foreground">
     <CardHeader>
         <CardTitle>Skills</CardTitle>
         <CardAction>
-          <Button variant="outline" size="icon" className="rounded-full text-primary-turquoise-foreground! border-primary-turquoise-foreground! hover:bg-primary-turquoise-foreground/20! cursor-pointer">
+          <Button variant="outline" size="icon" className="rounded-full text-primary-turquoise-foreground! border-primary-turquoise-foreground! hover:bg-primary-turquoise-foreground/20! cursor-pointer" onClick={showAlert}>
             <ArrowUpRightIcon />
           </Button>
         </CardAction>
