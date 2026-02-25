63d0457f07c8978a9e5ac7167766214a3b7bf885
Sarthak Singh <140454190+self-sasi@users.noreply.github.com>
2026-02-25 01:01:45 -0700

    update expired x social link

--- src/constants/socials.ts
+++ src/constants/socials.ts
@@ -4,6 +4,6 @@ export const SOCIALS: SocialIcon[] = [
   { network: "github", url: "https://github.com/self-sasi" },
   { network: "instagram", url: "https://www.instagram.com/bifunction/" },
   { network: "discord", url: "https://discord.gg/lmao" },
-  { network: "x", url: "https://x.com/tilde_key" },
+  { network: "x", url: "https://x.com/selfsasi" },
   { network: "linkedin", url: "https://linkedin.com/in/sarthaksingh0512" },
 ];
